2d4f340569c6576c7cdd63689e9f75927c8a4eaf
Matteo Ronchi <matteo.ronchi@gmail.com>
2015-12-03 10:22:30 +0100

    Added jsdom based helper to init emulated DOM environment

--- test/helpers/_document.js
+++ test/helpers/_document.js
@@ -1,3 +1,19 @@
-if (typeof document === 'undefined') {
-  global.document = {};
+// import ExecutionEnvironment from 'react/lib/ExecutionEnvironment';
+import jsdom from 'jsdom';
+
+export default function jsdomReact() {
+  const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+  const win = doc.defaultView;
+  global.document = doc;
+  global.window = win;
+  global.navigator = win.navigator;
+
+  const globalKeys = Object.keys(global);
+  Object.keys(win).forEach( key => {
+    if ( !globalKeys.find( gKey => gKey === key ) ) {
+      global[key] = win[key];
+    }
+  });
+
+  // ExecutionEnvironment.canUseDOM = true;
 }
